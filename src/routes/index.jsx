f0c4de99fe0119762079ec33ecd1211900294801
thombedej <thombedej@gmail.com>
2023-05-01 13:42:12 +0200

    routing fix

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,4 +1,5 @@
 import App from '../App';
+import Redirect from '../pages/Redirect';
 
 export default [
   {
@@ -7,11 +8,6 @@ export default [
   },
   {
     path: 'joinourjourney',
-    element: (function () {
-      window.location.replace(
-        'https://sommify.notion.site/Make-wine-easy-with-sommifyAI-e0abba39d0e44f488c78e231d115d014'
-      );
-      return null;
-    })(),
+    element: <Redirect />,
   },
 ];
