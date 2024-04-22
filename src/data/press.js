269c0a6e2a357e4a5e4e91d4cff79b1bb3f3db21
thombedej <thombedej@gmail.com>
2024-04-22 18:17:25 +0200

    fix some stuff

---
+++ src/data/press.js
@@ -0,0 +1,43 @@
+import Decanter from '../assets/press/decanter.svg';
+import HS from '../assets/press/hs.png';
+import Kauppalehti from '../assets/press/kl.svg';
+import Silta from '../assets/press/silta.svg';
+import TS from '../assets/press/t&s.svg';
+
+export default [
+  {
+    src: Decanter,
+    alt: '',
+    width: 'auto',
+    height: 120,
+    href: 'https://www.decanter.com/wine/ai-and-wine-a-taste-of-the-future-523210/',
+  },
+  {
+    src: Kauppalehti,
+    alt: '',
+    width: 'auto',
+    height: 30,
+    href: 'https://www.kauppalehti.fi/uutiset/suomalaisyhtio-sai-67-miljardin-myyntia-tekevan-kauppajatin-ensimmaiseksi-maksavaksi-asiakkaakseen/8409b91d-2bd7-4cfc-8057-20c889a4b66b',
+  },
+  {
+    src: HS,
+    alt: '',
+    width: 'auto',
+    height: 120,
+    href: 'https://www.hs.fi/visio/art-2000010003755.html',
+  },
+  {
+    src: Silta,
+    alt: '',
+    width: 'auto',
+    height: 20,
+    href: 'https://blog.siltahouse.com/how-to-survive-in-san-francisco-as-a-founder-the-complete-survival-guide/',
+  },
+  {
+    src: TS,
+    alt: '',
+    width: 'auto',
+    height: 40,
+    href: 'https://www.tekniikkatalous.fi/uutiset/suomalaisyhtion-johtaja-paatti-soittaa-67000-m-liikevaihtoa-tekevalle-maailman-suurimmalle-viinimyyjalle-heti-nappasi-maksavaksi-asiakkaaksi/196b7cdf-32bf-4223-9412-7968321f4e9e',
+  },
+];
