0d3d940ed35735c3db50c4ae6a37fc1bb119bf78
thombedej <thombedej@gmail.com>
2025-03-07 18:18:04 +0100

    calendly

--- src/components/ModalBooking.jsx
+++ src/components/ModalBooking.jsx
@@ -1,9 +1,10 @@
-import { motion } from 'framer-motion';
 import CustomModal from './CustomModal';
+import { InlineWidget } from 'react-calendly';
 
 export default function ModalBooking({ isOpen, close }) {
   return (
     <CustomModal
+      noBody
       isOpen={isOpen}
       close={close}
       style={{
@@ -12,10 +13,11 @@ export default function ModalBooking({ isOpen, close }) {
         padding: 0,
       }}
     >
-      <iframe
+      {/* <iframe
         src='https://calendar.google.com/calendar/u/0/appointments/AcZssZ2mzIg5lKAVFzLu7R4umL4rgYYr7WV8yIOHwA8=?gv=true'
         style={{ width: '100%', height: '100%', border: 'none' }}
-      />
+      /> */}
+      <InlineWidget url='https://calendly.com/sommifyai?hide_gdpr_banner=1' />
     </CustomModal>
   );
 }
